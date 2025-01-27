814c040decbc7e1ca941eb230613f034e99656e8
rafieaswandi <muhammadrafieaswandi@gmail.com>
2025-01-27 11:36:35 +0700

    third commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// const themeButton = document.getElementById("theme-toggle");
+
+// themeButton.addEventListener("click", () => {
+//     document.documentElement.classList.toggle("dark");
+//     if document.documentElement.classList.contains("dark") {
+//         localStorage.theme = "dark";
+//     } else {
+//         localStorage.theme = "light";
+//     }
+// });
+
